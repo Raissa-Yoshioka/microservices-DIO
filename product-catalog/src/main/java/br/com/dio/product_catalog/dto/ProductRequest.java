d5cd281ff3b2e51186e5747b5ec818b2e1934923
Raissa Rinaldi Yoshioka <rryoshioka@gmail.com>
2025-08-16 07:36:13 -0400

    Update product-catalog

--- product-catalog/src/main/java/br/com/dio/product_catalog/dto/ProductRequest.java
+++ product-catalog/src/main/java/br/com/dio/product_catalog/dto/ProductRequest.java
@@ -2,10 +2,19 @@ package br.com.dio.product_catalog.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 
 public record ProductRequest(
-    @NotBlank String name,
-    @NotBlank String description,
-    @NotNull double price
+    Long id,
+
+    @NotBlank(message = "O campo 'nome' percisa estar preenchido") 
+    String name,
+
+    @NotBlank(message = "O campo 'descrição' precisa estar preenchido")
+    String description,
+
+    @NotNull 
+    @Positive
+    double price
 ) {
 }
