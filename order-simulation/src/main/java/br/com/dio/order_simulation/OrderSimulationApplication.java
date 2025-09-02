3f263b4eb2f050db86a7fd74c32cc8e207a7ef33
Raissa Rinaldi Yoshioka <rryoshioka@gmail.com>
2025-09-02 13:57:18 -0400

    Creation of order-simulation microservice

---
+++ order-simulation/src/main/java/br/com/dio/order_simulation/OrderSimulationApplication.java
@@ -0,0 +1,13 @@
+package br.com.dio.order_simulation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderSimulationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderSimulationApplication.class, args);
+	}
+
+}
---
+++ order-simulation/src/test/java/br/com/dio/order_simulation/OrderSimulationApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dio.order_simulation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderSimulationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
